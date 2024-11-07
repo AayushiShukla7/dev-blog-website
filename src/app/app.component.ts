cd1af2dea03999c5745fc476e04bd4ba06900bf8
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-11-07 17:59:56 -0500

    Subscription form design completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,9 @@ import { FooterComponent } from './layouts/footer/footer.component';
     RouterOutlet,
     HeaderComponent,
     CategoryNavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PostCardComponent,
+    SubscriptionFormComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

23fbe94ab8b8f984d6e98d64ea7581b712d51d7d
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-11-07 18:12:52 -0500

    Featured Posts section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
-import { PostCardComponent } from './layouts/post-card/post-card.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
@@ -14,7 +13,6 @@ import { SubscriptionFormComponent } from './subscription-form/subscription-form
     HeaderComponent,
     CategoryNavbarComponent,
     FooterComponent,
-    PostCardComponent,
     SubscriptionFormComponent
   ],
   templateUrl: './app.component.html',
